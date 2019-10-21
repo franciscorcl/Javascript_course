464390b13f0332959ce9ee10a99fe6fc727975e9
Francisco Leal <franciscorcl@gmail.com>
2019-10-21 19:26:00 -0300

    Seção 8 - Node.JS

---
+++ 8_node/121_node/moduloCliente.js
@@ -0,0 +1,11 @@
+//Importanto módulos
+const modulaA = require('./moduloA.js.js')
+const modulaB = require('./moduloB')
+
+console.log(modulaA.ola)
+console.log(modulaA.bemVindo)
+console.log(modulaA.ateLogo)
+console.log(modulaA)
+console.log(modulaB.bomDia)
+console.log(modulaB.boaNoite)
+console.log(modulaB)
---
+++ 8_node/123_Require/pastaB/acessarModulos.js
@@ -0,0 +1,14 @@
+const moduloA = require('../../122_node/moduloA')
+
+console.log(moduloA.ola)
+
+const c = require('./pastC')
+
+console.log(c.ola2)
+
+
+// const http = require('http')
+// http.createServer((req, res) => {
+//     res.write('Bom dia!')
+//     res.end()
+// }).listen(8080)
